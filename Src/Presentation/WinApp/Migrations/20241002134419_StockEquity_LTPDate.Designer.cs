291a758d66c42e0a338d7e1dbfaf5298626227dd
Hoshiyar Singh <hoshiyar.singh@LIVE.COM>
2024-10-02 19:15:28 +0530

    LTP Date

--- Src/Presentation/WinApp/Migrations/20241002134419_StockEquity_LTPDate.Designer.cs
+++ Src/Presentation/WinApp/Migrations/20241002134419_StockEquity_LTPDate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShareMarket.WinApp.Store;
 
@@ -11,9 +12,11 @@ using ShareMarket.WinApp.Store;
 namespace ShareMarket.WinApp.Migrations
 {
     [DbContext(typeof(ShareMarketContext))]
-    partial class ShareMarketContextModelSnapshot : ModelSnapshot
+    [Migration("20241002134419_StockEquity_LTPDate")]
+    partial class StockEquity_LTPDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Src/Presentation/WinApp/Migrations/20241002134419_StockEquity_LTPDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShareMarket.WinApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class StockEquity_LTPDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "LTPDate",
+                table: "EquityStocks",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LTPDate",
+                table: "EquityStocks");
+        }
+    }
+}
